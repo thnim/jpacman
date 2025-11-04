dc28641c2f408e2aae0bd172aae62f7aae7cc2fe
thnim <nimmida.man@student.mahidol.ac.th>
2025-11-04 15:45:15 +0700

    add BoardTest cases

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Tests for {@link Board}.
+ * Tests to confirm {@link Board}.
  */
 public class BoardTest {
     /**
